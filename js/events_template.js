d16e9ae9007721928d722db4d251f740319165c1
Florian Katenbrink <f.katenbrink@gmail.com>
2013-12-03 16:04:33 +0100

    JSON von der js getrennt

--- js/events_template.js
+++ js/events_template.js
@@ -1,29 +1,31 @@
-var eventTemplate =
-"\
-{{#each eventData}}\
-	<h2 id=\"{{tag}}\">{{category}}</h2>\
-	{{#each events}}\
-		<div class=\"well\">\
-			<h4>{{title}}</h4>\
-			<p><i>\
-				Wann: {{time}}<br>\
-				Wo: {{place}}\
-			</i></p>\
-			<p>{{{text}}}</p>\
-		</div>\
+$.getJSON('json/events.json', function(json) {
+	var eventTemplate =
+	"\
+	{{#each .}}\
+		<h2 id=\"{{tag}}\">{{category}}</h2>\
+		{{#each events}}\
+			<div class=\"well\">\
+				<h4>{{title}}</h4>\
+				<p><i>\
+					Wann: {{time}}<br>\
+					Wo: {{place}}\
+				</i></p>\
+				<p>{{{text}}}</p>\
+			</div>\
+		{{/each}}\
 	{{/each}}\
-{{/each}}\
-";
+	";
 
-var compiledEventTemplate = Handlebars.compile(eventTemplate);
-document.getElementById('events').innerHTML = compiledEventTemplate(eventTemplateData);
+	var compiledEventTemplate = Handlebars.compile(eventTemplate);
+	document.getElementById('events').innerHTML = compiledEventTemplate(json);
 
-var sidebarTemplate =
-"\
-{{#each eventData}}\
-	<a class=\"list-group-item\" href=\"#{{tag}}\">{{category}}</a>\
-{{/each}}\
-";
+	var sidebarTemplate =
+	"\
+	{{#each .}}\
+		<a class=\"list-group-item\" href=\"#{{tag}}\">{{category}}</a>\
+	{{/each}}\
+	";
 
-var compiledSidebarTemplate = Handlebars.compile(sidebarTemplate);
-document.getElementById('sidebar').innerHTML = compiledSidebarTemplate(eventTemplateData);
+	var compiledSidebarTemplate = Handlebars.compile(sidebarTemplate);
+	document.getElementById('sidebar').innerHTML = compiledSidebarTemplate(json);
+});
--- js/offers_template.js
+++ js/offers_template.js
@@ -1,29 +1,31 @@
-var offerTemplate =
-"\
-{{#each offerData}}\
-	<h2 id=\"{{tag}}\">{{category}}</h2>\
-	{{#each offers}}\
-		<div class=\"well\">\
-			<h4>{{title}}</h4>\
-			<p><i>\
-				Wann: {{time}}<br>\
-				Wo: {{place}}\
-			</i></p>\
-			<p>{{{text}}}</p>\
-		</div>\
+$.getJSON('json/offers.json', function(json) {
+	var offerTemplate =
+	"\
+	{{#each .}}\
+		<h2 id=\"{{tag}}\">{{category}}</h2>\
+		{{#each offers}}\
+			<div class=\"well\">\
+				<h4>{{title}}</h4>\
+				<p><i>\
+					Wann: {{time}}<br>\
+					Wo: {{place}}\
+				</i></p>\
+				<p>{{{text}}}</p>\
+			</div>\
+		{{/each}}\
 	{{/each}}\
-{{/each}}\
-";
+	";
 
-var compiledOfferTemplate = Handlebars.compile(offerTemplate);
-document.getElementById('offers').innerHTML = compiledOfferTemplate(offerTemplateData);
+	var compiledOfferTemplate = Handlebars.compile(offerTemplate);
+	document.getElementById('offers').innerHTML = compiledOfferTemplate(json);
 
-var sidebarTemplate =
-"\
-{{#each offerData}}\
-	<a class=\"list-group-item\" href=\"#{{tag}}\">{{category}}</a>\
-{{/each}}\
-";
+	var sidebarTemplate =
+	"\
+	{{#each .}}\
+		<a class=\"list-group-item\" href=\"#{{tag}}\">{{category}}</a>\
+	{{/each}}\
+	";
 
-var compiledSidebarTemplate = Handlebars.compile(sidebarTemplate);
-document.getElementById('sidebar').innerHTML = compiledSidebarTemplate(offerTemplateData);
+	var compiledSidebarTemplate = Handlebars.compile(sidebarTemplate);
+	document.getElementById('sidebar').innerHTML = compiledSidebarTemplate(json);
+});
